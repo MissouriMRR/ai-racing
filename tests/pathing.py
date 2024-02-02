a818f9cc179c59724390acb3a161e838f4855828
Nate Nevois <nnnevwa630@gmail.com>
2024-02-01 19:06:22 -0600

    Added some docstrings

--- tests/pathing.py
+++ tests/pathing.py
@@ -1,80 +1,175 @@
+"""
+This file starts an airsimdroneracinglab environment,
+initializes a user drone and a baseline drone,
+then autonomously controls the drone using a barebones pathing algorithm.
+Many features are hard-coded at times
+and the drone's movement is rather choppy,
+but this file offers a good starting point for more complex pathing algorithms.
+"""
+
+import math
 import threading
 import time
-import math
 import airsimdroneracinglab
 
 
 class ReproduceResetRaceCondition:
     """
-    Contains vaious functions for interacting with the ADRL unreal environment
+    Contains various functions for interacting with the ADRL unreal environment.
+
+    Attributes
+    ----------
+    airsim_client : airsimdroneracinglab.client.MultirotorCleint
+        Default airsim client for interacting with the environment
+    airsim_client_2 : airsimdroneracinglab.client.MultirotorCleint
+        Second alternative airsim client for interacting with the environment
+    airsim_client_3 : airsimdroneracinglab.client.MultirotorCleint
+        Third alternative airsim client for interacting with the environment
+    drone_name : string
+        Name of user controlled drone.
+    is_thread_active : boolean
+        Boolean used to see if current thread is active or not
+    thread_reset : threading.Thread
+        Thread used to reset airsim cleint.
+    thread_reset_race : threading.Thread
+        Thread used to reset current race
+    thread_reset_and_reset_race : threading.Thread
+        Thread used to reset airsim client and current race
+    level_name : string
+        Name of current loaded level (see load_level() for options)
+
+    Methods
+    -------
+    repeat_timer(callback, period: float)
+        Simple sleep timer
+    load_level(level_name: str, sleep_sec: float = 2.0)
+        Loads given simulator level
+    reset()
+        Resets airsim client
+    reset_race()
+        Resets current race
+    reset_and_reset_race()
+        Resets airsim cleint and current race
+    start_race(tier: int = 1)
+        Starts race against a baseline drone using moveonspline
+    initialize_drone()
+        Initializes user drone, enabling API control and arming the vehicle.
+        Sets default values for trajectory tracker gains.
+    start_threads()
+        Starts threads if not already active.
+    stop_threads()
+        Stops threads if not already stopped
+    takeoff()
+        Built-in ADRL async takeoff function.
+        Sends moveonspline commands to drone in order to take off.
+    give_control_stick_inputs(self, roll, pitch, yaw, z, duration)
+        Reads various control stick inputs from command-line interface
+        and passes them to the drone for a given amount of time.
+    get_gate_pose(gate_index)
+        Gets a list of gates from the airsim client,
+        then returns the pose of the gate requested in the form of a vector3r.
+    get_drone_pose()
+        Gets the current position of the drone from the airsim client and returns it as a vector3r.
     """
 
-    def __init__(self, drone_name="drone_1"):
-        self.airsim_client = airsimdroneracinglab.MultirotorClient()
-        self.airsim_client_2 = airsimdroneracinglab.MultirotorClient()
-        self.airsim_client_3 = airsimdroneracinglab.MultirotorClient()
-        self.drone_name = drone_name
-        self.is_thread_active = False
-        self.thread_reset = threading.Thread(
+    def __init__(self, drone_name: str = "drone_1") -> None:
+        """
+        Initializes class variables.
+
+        Parameters
+        ----------
+            drone_name : string
+                Name of user controlled drone (defaults to 'drone_1')
+        """
+        self.airsim_client: airsimdroneracinglab.client.MultirotorClient = (
+            airsimdroneracinglab.MultirotorClient()
+        )
+        self.airsim_client_2: airsimdroneracinglab.client.MultirotorClient = (
+            airsimdroneracinglab.MultirotorClient()
+        )
+        self.airsim_client_3: airsimdroneracinglab.client.MultirotorClient = (
+            airsimdroneracinglab.MultirotorClient()
+        )
+        self.drone_name: str = drone_name
+        self.is_thread_active: bool = False
+        self.thread_reset: threading.Thread = threading.Thread(
             target=self.repeat_timer, args=(self.reset, 0.05)
         )
-        self.thread_reset_race = threading.Thread(
+        self.thread_reset_race: threading.Thread = threading.Thread(
             target=self.repeat_timer, args=(self.reset_race, 0.03)
         )
-        self.thread_reset_and_reset_race = threading.Thread(
+        self.thread_reset_and_reset_race: threading.Thread = threading.Thread(
             target=self.repeat_timer, args=(self.reset_and_reset_race, 0.09)
         )
         self.is_thread_active = False
 
-    def repeat_timer(self, callback, period):
-        """Simple sleep timer"""
+    def repeat_timer(self, callback, period: float) -> None:
+        """
+        Simple sleep timer.
+
+        Parameters
+        ----------
+            callback
+                Function to call
+            period : float
+                Repeat interval in seconds
+        """
         while self.is_thread_active:
             callback()
             time.sleep(period)
 
-    def load_level(self, level_name, sleep_sec=2.0):
+    def load_level(self, level_name: str, sleep_sec: float = 2.0) -> None:
         """
-        Loads simulator level.
+        Loads given simulator level.
 
-        Args:
-        -----
-            level_name (string): Soccer_Field_Easy, Soccer_Field_Medium, ZhangJiaJie_Medium,
-            Building99_Hard, Qualification_Tier_1, Qualification_Tier_2, Qualification_Tier_3,
-            Final_Tier_1, Final_Tier_2, or Final_Tier_3
-            sleep_sec (float, optional): Sleep time for loading level. Defaults to 2.0.
+        Parameters
+        ----------
+            level_name : string
+                Soccer_Field_Easy, Soccer_Field_Medium, ZhangJiaJie_Medium,
+                Building99_Hard, Qualification_Tier_1, Qualification_Tier_2,
+                Qualification_Tier_3, Final_Tier_1, Final_Tier_2, or Final_Tier_3
+            sleep_sec : float, default=2.0
+                Sleep time for loading level.
         """
-        self.level_name = level_name
+        self.level_name: str = level_name
         self.airsim_client.simLoadLevel(self.level_name)
         self.airsim_client.confirmConnection()  # failsafe
         time.sleep(sleep_sec)  # let the environment load completely
 
-    def reset(self):
+    def reset(self) -> None:
         """Resets Airsim cleint."""
         print(time.time(), "called reset")
         self.airsim_client.reset()
 
-    def reset_race(self):
+    def reset_race(self) -> None:
         """Resets current race."""
         print(time.time(), "called simResetRace")
         self.airsim_client_2.simResetRace()
 
-    def reset_and_reset_race(self):
-        """Resets Airsim cleint and current race"""
+    def reset_and_reset_race(self) -> None:
+        """Resets airsim cleint and current race"""
         print(time.time(), "called reset, followed by simResetRace")
         self.airsim_client_3.reset()
         self.airsim_client_3.simResetRace()
 
-    def start_race(self, tier):
+    def start_race(self, tier: int = 1) -> None:
+        """
+        Starts race against a baseline drone using moveonspline
+
+        Parameters
+        ----------
+            tier : int, default=1
+                Race tier determining difficulty.
+                Tier 1: Planning only; Tier 2: Perception Only; Tier 3: Planning and Perception.
+        """
         print(time.time(), "called start race")
         self.airsim_client.simStartRace(tier)
 
-    def initialize_drone(self):
+    def initialize_drone(self) -> None:
         """
         Initializes user drone, enabling API control and arming the vehicle.
         Sets default values for trajectory tracker gains.
-
         """
-
         self.airsim_client.enableApiControl(vehicle_name=self.drone_name)
         self.airsim_client.arm(vehicle_name=self.drone_name)
 
@@ -101,7 +196,7 @@ class ReproduceResetRaceCondition:
         )
         time.sleep(0.2)
 
-    def start_threads(self):
+    def start_threads(self) -> None:
         """Starts threads if not already active."""
         if not self.is_thread_active:
             self.is_thread_active = True
@@ -110,8 +205,8 @@ class ReproduceResetRaceCondition:
             self.thread_reset_and_reset_race.start()
             print("Started threads")
 
-    def stop_threads(self):
-        """Stops threads if not already stopped"""
+    def stop_threads(self) -> None:
+        """Stops threads if not already stopped."""
         if self.is_thread_active:
             self.is_thread_active = False
             self.thread_reset.join()
@@ -119,31 +214,38 @@ class ReproduceResetRaceCondition:
             self.thread_reset_and_reset_race.join()
             print("Stopped threads.")
 
-    def takeoff(self, drone_orientation):
-        """Built-in ADRL async takeoff function"""
-        self.airsim_client.moveByRollPitchYawThrottleAsync(
-            0,
-            0.1,
-            drone_orientation[2] + math.pi,
-            0.65,
-            1,
-            vehicle_name=self.drone_name,
-        )
-        time.sleep(1)
-
-    def give_control_stick_inputs(self, roll, pitch, yaw, z, duration):
+    def takeoff(self) -> None:
         """
-        Reads various control stick inputs passes them to the drone for a given amount of time.
+        Built-in ADRL async takeoff function.
+        Sends moveonspline commands to drone in order to take off.
+        """
+        self.airsim_client.takeoffAsync().join()
 
-        Args:
-            roll (float): an angle in radians/s
-            pitch (float): an angle in radians/s
-            yaw (float): an angle in radians/s
-            z (float): desired z height
-            duration (float): in seconds
+    def give_control_stick_inputs(
+        self, roll: float, pitch: float, yaw: float, z: float, duration: float
+    ) -> None:
+        """
+        Reads various control stick inputs from command-line interface
+        and passes them to the drone for a given amount of time.
+        Drone automatically attempts to stabilize itself once commands are finished.
 
-        Notes (from airsimdroneracinglab documenation):
-        -----------------------------------------------
+        Parameters
+        ----------
+            roll : float
+                Roll angle to be passed to drone given in radians
+            pitch : float
+                Pitch angle to be passed to drone given in radians
+            yaw : float
+                Yaw angle to be passed to drone given in radians
+            z : float
+                A z (height) value to be passed to the drone given relative to the ground
+                Drone automatically travels to this position, smoothing its movement automatically
+            duration : float
+                Duration for the inputs to be passed to the drone given in seconds
+
+        Notes
+        -----
+            (from airsimdroneracinglab documenation)
             Roll angle, pitch angle, and yaw angle are given in radians, in the body frame.
             The body frame follows the Front Left Up (FLU) convention, and right-handedness.
         """
@@ -151,19 +253,40 @@ class ReproduceResetRaceCondition:
             roll, pitch, yaw, z, duration, vehicle_name=self.drone_name
         )
 
-    def get_gate_pose(self, gate_number):
+    def get_gate_pose(self, gate_index: int):
         """
-        Returns the pose of the given gate in the form of a vector3r.
+        Gets a list of gates from the airsim client,
+        then returns the pose of the gate requested in the form of a vector3r.
 
-        Args:
-            gate_number (int): Number of gate (starts at 0).
+        Paremeters
+        ----------
+            gate_index : int
+                Index of requested/next gate (starts at 0)
+
+        Returns
+        -------
+            gate_position : airsimdroneracinglab.vector3r
+                Position of the next gate given as a vector3r
+
         """
-        gate_list = self.airsim_client.simListSceneObjects("Gate.*")
-        gate_pose = self.airsim_client.simGetObjectPose(gate_list[gate_number])
-        return gate_pose
+        gate_list: list[str] = self.airsim_client.simListSceneObjects("Gate.*")
+        gate_position: airsimdroneracinglab.Vector3r = (
+            self.airsim_client.simGetObjectPose(gate_list[gate_index])
+        )
+        return gate_position
 
     def get_drone_pose(self):
-        return self.airsim_client.simGetObjectPose(self.drone_name)
+        """
+        Gets the current position of the drone from the airsim client and returns it as a vector3r
+
+        Returns
+        -------
+            drone_position : airsimdroneracinglab.Vector3r
+        """
+        drone_position: airsimdroneracinglab.Vector3r = (
+            self.airsim_client.simGetObjectPose(self.drone_name)
+        )
+        return drone_position
 
 
 def generate_vector(start_pos, end_pos):
@@ -242,10 +365,10 @@ if __name__ == "__main__":
     drone_orientation = airsimdroneracinglab.utils.to_eularian_angles(
         drone_pose.orientation
     )
-    reproducer.takeoff(drone_orientation)
+    reproducer.takeoff()
 
     for i in range(25):
-        duration = 0.1  # Time interval for giving drone commands (in seconds)
+        input_duration = 0.1  # Time interval for giving drone commands (in seconds)
 
         drone_pose = reproducer.get_drone_pose()
         gate_pose = reproducer.get_gate_pose(i)
@@ -265,7 +388,7 @@ if __name__ == "__main__":
             )
             vector_to_gate = generate_vector(drone_pose.position, gate_pose.position)
             if math.isnan(vector_to_gate[0]):
-                time.sleep(duration)
+                time.sleep(input_duration)
                 continue
             yaw_angle = generate_yaw_angle(vector_to_gate)
             print("Drone Orientation:", drone_orientation)
@@ -279,11 +402,11 @@ if __name__ == "__main__":
                 roll_angle = 0
             print("Roll angle:", roll_angle)
             reproducer.give_control_stick_inputs(
-                roll_angle, 0.02, yaw_angle, gate_pose.position.z_val, duration
+                roll_angle, 0.02, yaw_angle, gate_pose.position.z_val, input_duration
             )
             distance_to_gate = get_distance_to_gate(vector_to_gate, i)
             print()
 
             if distance_to_gate < 1:
                 location_reached = True
-            time.sleep(duration)
+            time.sleep(input_duration)
