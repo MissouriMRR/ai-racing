e0a8c79a9b48ff904fa099591c6744b276b3b349
Nate Nevois <nnnevwa630@gmail.com>
2024-03-05 17:40:12 -0600

    Implemented gps velocity tracking for more sensible calculations

--- tests/pathing_pid.py
+++ tests/pathing_pid.py
@@ -69,9 +69,9 @@ class ReproduceResetRaceCondition:
         then returns the pose of the gate requested in the form of an object.
     get_drone_pose()
         Gets the current position of the drone from the airsim client and returns it as an object.
-    get_drone_y_velocity()
-        Calculates the drone's Y velocity using the difference in it's position over
-        a fixed interval of time.
+    get_drone_velocity_components()
+        Calculates the perpendicular x, y, and z components of the drone using the change
+        in the drone's position over time and trigonometric functions.
     get_num_gates()
         Gets the total number of gates in the active level from the airsim client.
     """
@@ -302,91 +302,69 @@ class ReproduceResetRaceCondition:
         )
         return drone_position
 
-    def get_drone_y_velocity(self) -> float:
+    def get_drone_velocity_components(
+        self,
+        yaw_angle: float,
+        x_velocity_deadzone: float = 0.05,
+        y_velocity_deadzone: float = 0.05,
+        z_velocity_deadzone: float = 0.05,
+    ) -> tuple[float, float, float]:
         """
-        Calculates the drone's Y velocity using the difference in it's position over
-        a fixed interval of time.
+        Calculates the perpendicular x, y, and z components of the drone using gps data
+        and trigonometric functions.
+
+        Parameters
+        ----------
+            yaw_angle : float
+                The current yaw angle of the drone
+            x_velocity_deadzone : float
+                Values between this value +- returned as 0 (defaults to .05)
+            y_velocity_deadzone : float
+                Values between this value +- returned as 0 (defaults to .05)
+            z_velocity_deadzone : float
+                Values between this value +- returned as 0 (defaults to .05)s
 
         Returns
         -------
-            y_velocity : float
-                Drone's current y velocity (perpendicular left-right velocity) in relation to frame
+            velocity_components : tuple[float, float, float]
+                Contains the drones current x, y, and z velocity components, respectively
         """
-        interval: float = 0.01
-        velocity_deadzone: float = (
-            0.15  # Adjusting this value may result in better/worse performance
+
+        # Gets drone's velocity from GPS data with respect to the ground
+        velocity_vector: airsimdroneracinglab.Vector3r = (
+            self.airsim_client.getGpsData().gnss.velocity
         )
 
-        # Gets drone's positon "interval" number of seconds apart
-        old_position: airsimdroneracinglab.Pose = self.get_drone_pose()
-        time.sleep(interval)
-        new_position: airsimdroneracinglab.Pose = self.get_drone_pose()
+        # Performs trig operations to translate vectors to be perpendicular with respect to drone
+        x_velocity: float = (
+            velocity_vector.y_val * math.cos(yaw_angle + (math.pi / 2))
+        ) + (velocity_vector.x_val * math.cos(yaw_angle))
 
-        # Gets difference in the new and old position divided by interval to get velocity vectors
-        y_vector: float = (new_position.position.y_val - old_position.position.y_val) / interval
-        x_vector: float = -((new_position.position.x_val - old_position.position.x_val) / interval)
+        y_velocity: float = (
+            velocity_vector.y_val * math.sin(yaw_angle + (math.pi / 2))
+        ) + (velocity_vector.x_val * math.sin(yaw_angle))
 
-        # Gets the drone's current objective yaw angle as tuple[roll, pitch, yaw]
-        orientation: tuple[float, float, float] = airsimdroneracinglab.utils.to_eularian_angles(
-            new_position.orientation
-        )
-        yaw_angle: float = orientation[2]
-
-        # Gets the y (perpendicular) component of the objective x and y velocity vectors
-        x_vector_y_component: float = x_vector / math.cos(yaw_angle)
-        y_vector_y_component = y_vector / math.cos(
-            yaw_angle - (math.pi / 2)
-        )  # -pi/2 to shift -90 degrees
-
-        # Adds the two y componenets together to get the drone's total y velocity
-        y_velocity = x_vector_y_component + y_vector_y_component
-
-        # Fixes trig sign errors
-        if 0 > yaw_angle > -(math.pi / 2):
-            y_velocity = -y_velocity
-        elif math.pi > yaw_angle > (math.pi / 2):
-            y_velocity = -y_velocity
+        z_velocity: float = -velocity_vector.z_val
 
         # if y velocity is inside deadzone set it to 0
-        if velocity_deadzone > y_velocity > -velocity_deadzone:
+        if x_velocity_deadzone > x_velocity > -x_velocity_deadzone:
+            x_velocity = 0
+        if y_velocity_deadzone > y_velocity > -y_velocity_deadzone:
             y_velocity = 0
+        if z_velocity_deadzone > z_velocity > -z_velocity_deadzone:
+            z_velocity = 0
 
-        print(f"Drone Yaw Angle is: {yaw_angle}")
+        print(f"Drone X Velocity is: {x_velocity}")
         print(f"Drone Y Velocity is: {y_velocity}")
-        return y_velocity
-
-    def get_drone_z_velocity(self) -> float:
-        """
-        Calculates the drone's z velocity using the difference in it's position over
-        a fixed interval of time.
-
-        Returns
-        -------
-            z_velocity : float
-                Drone's current z velocity (perpendicular up-down velocity) in relation to frame
-        """
-        interval: float = 0.01
-        # velocity_deadzone: float = (
-        #     0.15  # Adjusting this value may result in better/worse performance
-        # )
-
-        # Gets drone's positon "interval" number of seconds apart
-        old_position: airsimdroneracinglab.Pose = self.get_drone_pose()
-        time.sleep(interval)
-        new_position: airsimdroneracinglab.Pose = self.get_drone_pose()
-
-        # Gets difference in the new and old position divided by interval to get velocity vectors
-        z_vector: float = (new_position.position.z_val - old_position.position.z_val) / interval
-
-        # Should be shifted to be truly perpendicular to drone for the future
-        z_velocity: float = -z_vector
-
-        # # if z velocity is inside deadzone set it to 0
-        # if velocity_deadzone > y_velocity > -velocity_deadzone:
-        #     y_velocity = 0
-
         print(f"Drone Z Velocity is: {z_velocity}")
-        return z_velocity
+
+        velocity_components: tuple[float, float, float] = (
+            x_velocity,
+            y_velocity,
+            z_velocity,
+        )
+
+        return velocity_components
 
     def get_num_gates(self) -> int:
         """
@@ -521,7 +499,9 @@ def generate_vector(
             Vector between the two points given
     """
     x_distance: float = start_pos.x_val - end_pos.x_val
-    y_distance: float = -(start_pos.y_val - end_pos.y_val)  # Inverts Y value for proper result
+    y_distance: float = -(
+        start_pos.y_val - end_pos.y_val
+    )  # Inverts Y value for proper result
     z_distance: float = start_pos.z_val - end_pos.z_val
     vector_difference: tuple[float, float, float] = (x_distance, y_distance, z_distance)
     print("Vector to Gate:", vector_difference)
@@ -575,60 +555,48 @@ def get_distance_to_target(target_vector: tuple[float, float, float]) -> float:
     return distance
 
 
-def calc_roll_angle(y_velocity: float) -> float:
-    """
-    Generates a roll angle that will somewhat correct the course of the drone to reduce
-    its y velocity.
-
-    Returns
-    -------
-        roll_angle : float
-            Roll angle needed to minimize the drone's y velocity
-    """
-    roll_angle: float
-    if y_velocity > 0:
-        roll_angle = -0.075 * (((abs(y_velocity) - 1) / (4 * abs(y_velocity))) + 2)
-    elif y_velocity < 0:
-        roll_angle = 0.075 * (((abs(y_velocity) - 1) / (4 * abs(y_velocity))) + 2)
-    else:
-        roll_angle = 0
-    print("Roll angle:", roll_angle)
-
-    return roll_angle
-
-
 if __name__ == "__main__":
     # Sets up race, initializes drone, loads level, and takes off
     reproducer = ReproduceResetRaceCondition("drone_1")
-    reproducer.load_level("Soccer_Field_Easy")  # Level name can be changed - see load_level()
+    reproducer.load_level(
+        "Soccer_Field_Easy"
+    )  # Level name can be changed - see load_level()
     NUM_GATES = 12  # There are 12 gates on the default level "Soccer_Field_Easy"
     reproducer.initialize_drone()
     reproducer.start_race(1)
 
     # Gets drone's orientation to take off in the right direction
     drone_pose: airsimdroneracinglab.Pose = reproducer.get_drone_pose()
-    drone_orientation = airsimdroneracinglab.utils.to_eularian_angles(drone_pose.orientation)
+    drone_orientation = airsimdroneracinglab.utils.to_eularian_angles(
+        drone_pose.orientation
+    )
     reproducer.takeoff(drone_orientation)
 
     # Gets the number of gates in the active level
     NUM_GATES = reproducer.get_num_gates()
     print("There are ", NUM_GATES, " gates present.")
 
-    throttle_PID: PID = PID(max_output=5)
+    throttle_PID: PID = PID(max_output=1)
     z_velocity_PID: PID = PID(max_output=0.4)
-    pitch_PID: PID = PID(max_output=0.1)
-    pitch_PID.set_target(1)
+    pitch_PID: PID = PID(kp=0.5, max_output=0.05)
+    pitch_PID.set_target(3)
+    roll_PID: PID = PID(kp=0.1, max_output=0.2)
+    roll_PID.set_target(0)
 
     # Iterates through each gate in the level
     for next_gate in range(NUM_GATES):
-        INPUT_DURATION: float = 0.1  # Time interval for giving drone commands (in seconds)
+        INPUT_DURATION: float = (
+            0.1  # Time interval for giving drone commands (in seconds)
+        )
         drone_pitch_angle: float = (
             0.02  # Currently a constant, should be calculated with a PID loop
         )
 
         # Gets next gate position and orientation
         gate_pose = reproducer.get_gate_pose(next_gate)
-        gate_orientation = airsimdroneracinglab.utils.to_eularian_angles(gate_pose.orientation)
+        gate_orientation = airsimdroneracinglab.utils.to_eularian_angles(
+            gate_pose.orientation
+        )
 
         throttle_PID.set_target(gate_pose.position.z_val)
 
@@ -648,22 +616,34 @@ if __name__ == "__main__":
                 time.sleep(INPUT_DURATION)
                 continue
 
-            # Caluclate inputs with PID loops
-            drone_z_velocity: float = reproducer.get_drone_z_velocity()
-            drone_z_velocity_input: float = -throttle_PID.adjust_output(drone_pose.position.z_val)
-            z_velocity_PID.set_target(drone_z_velocity_input)
-            drone_throttle_input: float = 0.5938 + z_velocity_PID.adjust_output(drone_z_velocity)
-            drone_x_velocity: float = 0.2
-            drone_pitch_angle = pitch_PID.adjust_output(drone_x_velocity)
+            # Calculates the drone's current velocity as 3 vectors perpendicular to its frame
+            drone_velocity_components: tuple[float, float, float] = (
+                reproducer.get_drone_velocity_components(
+                    -math.degrees(drone_orientation[2])
+                )
+            )
+            drone_x_velocity: float = drone_velocity_components[0]
+            drone_y_velocity: float = drone_velocity_components[1]
+            drone_z_velocity: float = drone_velocity_components[2]
 
-            # Calculates various angles needed to point the drone at the next gate
+            # Calculates various angles/inputs needed to get the drone through the next gate
+            drone_z_velocity_input: float = -throttle_PID.adjust_output(
+                drone_pose.position.z_val
+            )
+            z_velocity_PID.set_target(drone_z_velocity_input)
+            drone_throttle_input: float = 0.5938 + z_velocity_PID.adjust_output(
+                drone_z_velocity
+            )
+            drone_roll_angle = roll_PID.adjust_output(drone_y_velocity)
+            print("Roll: ", drone_roll_angle)
+            drone_pitch_angle = pitch_PID.adjust_output(drone_x_velocity)
+            print("Pitch: ", drone_pitch_angle)
             target_yaw_angle = generate_yaw_angle(vector_to_gate)
-            drone_y_velocity = reproducer.get_drone_y_velocity()
-            drone_roll_angle: float = calc_roll_angle(drone_y_velocity)
+
             # Sends inputs to the drone
             reproducer.give_control_stick_inputs(
                 drone_roll_angle,
-                drone_pitch_angle,
+                0.02,
                 target_yaw_angle,
                 drone_throttle_input,
                 INPUT_DURATION,
